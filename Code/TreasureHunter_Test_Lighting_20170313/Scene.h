5e43f58a191f2dd78ed8a39a328832c360fd834c
batherit <batherit0703@naver.com>
2017-03-13 04:25:13 +0900

    20170313_Test_Lighting
    - 기존 코드(20170313)에 조명을 추가함.
    - 라이팅 셰이더는 오브젝트의 재질을 상수 버퍼에 업데이트 함.
    - 씬은 조명을 위한 상수 버퍼를 생성함과 동시에 조명 데이터를 설정함.
    - 법선 벡터 계산하는 메서드에 문제 존재. => 법선 벡터가 중복되어 계산됨.
    - 셰이더, 래스터 라이져, 재질에 따라 오브젝트를 분류하여 상태 설정을 최소화할 필요가 있음.

---
+++ Code/TreasureHunter_Test_Lighting_20170313/Scene.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Object.h"			// 씬의 구성요소로 오브젝트들이 존재한다.
+#include "Camera.h"
+
+#define MAX_LIGHTS		4 
+#define POINT_LIGHT		1.0f
+#define SPOT_LIGHT		2.0f
+#define DIRECTIONAL_LIGHT	3.0f
+
+//1개의 조명을 표현하는 구조체이다. 
+struct LIGHT
+{
+	D3DXCOLOR m_d3dxcAmbient;
+	D3DXCOLOR m_d3dxcDiffuse;
+	D3DXCOLOR m_d3dxcSpecular;
+	D3DXVECTOR3 m_d3dxvPosition;
+	float m_fRange;
+	D3DXVECTOR3 m_d3dxvDirection;
+	float m_nType;
+	D3DXVECTOR3 m_d3dxvAttenuation;
+	float m_fFalloff;
+	float m_fTheta; //cos(m_fTheta)
+	float m_fPhi; //cos(m_fPhi)
+	float m_bEnable;
+	float padding;
+};
+
+//상수 버퍼는 크기가 반드시 16 바이트의 배수가 되어야 한다. 
+struct LIGHTS
+{
+	LIGHT m_pLights[MAX_LIGHTS];
+	D3DXCOLOR m_d3dxcGlobalAmbient;
+	D3DXVECTOR4 m_d3dxvCameraPosition;
+};
+
+class CScene
+{
+private:
+	CGameObject **m_ppObjects;
+	int m_nObjects;
+
+	LIGHTS *m_pLights;
+	ID3D11Buffer *m_pd3dcbLights;
+
+public:
+	CScene();
+	~CScene();
+
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	void BuildObjects(ID3D11Device *pd3dDevice);
+	void ReleaseObjects();
+
+	void CreateShaderVariables(ID3D11Device *pd3dDevice);
+	void UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, LIGHTS *pLights);
+	void ReleaseShaderVariables();
+
+	bool ProcessInput();
+	void AnimateObjects(float fTimeElapsed);
+	void Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera);		// 카메라 절두체 기법을 위하여 매개변수로 pCamera가 존재한다.
+};
+
+
