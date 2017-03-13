edb882dd42d01026281ea9db4f523e4073aa8866
batherit <batherit0703@naver.com>
2017-03-14 02:24:42 +0900

    TreasureHunter_Test_Texture_20170314
    텍스쳐 테스트 코드입니다.
    다음의 과정이 추가적으로 요구됩니다.
    1.  shader 자원 할당 메서드를 static 형태로 고칩니다.
    2. 부수적으로 생겨난 쓸데없는 코드 및 주석을 제거합니다.
    3. 법선 벡터를 새로이 계산합니다.
    4. 조명을 새로이 배치합니다.

---
+++ Code/TreasureHunter_Test_Texture_20170314/Scene.cpp
@@ -0,0 +1,236 @@
+#include "stdafx.h"
+#include "Scene.h"
+
+
+CScene::CScene()
+{
+	m_ppObjects = NULL;
+	m_nObjects = 0;
+
+	m_pLights = NULL;
+	m_pd3dcbLights = NULL;
+}
+
+CScene::~CScene()
+{
+}
+
+//void CScene::BuildObjects(ID3D11Device *pd3dDevice)
+//{
+//	//쉐이더 객체를 생성한다.
+//	CShader *pShader = new CShader();
+//	pShader->CreateShader(pd3dDevice);
+//
+//	//게임 객체에 대한 포인터들의 배열을 정의한다.
+//	m_nObjects = 1;
+//	m_ppObjects = new CGameObject*[m_nObjects];
+//
+//	//삼각형 메쉬(CTriangleMesh)를 생성한다.
+//	CTriangleMesh *pMesh = new CTriangleMesh(pd3dDevice);
+//
+//	//삼각형 객체(CTriangleObject)를 생성하고 삼각형 메쉬와 쉐이더를 연결한다.
+//	CRotatingObject *pObject = new CRotatingObject();
+//	pObject->SetMesh(pMesh);
+//	pObject->SetShader(pShader);
+//
+//	//객체를 저장한다.
+//	m_ppObjects[0] = pObject;
+//}
+
+//void CScene::BuildObjects(ID3D11Device *pd3dDevice)
+//{
+//	CShader *pShader = new CShader();
+//	pShader->CreateShader(pd3dDevice);
+//
+//	m_nObjects = 1;
+//	m_ppObjects = new CGameObject*[m_nObjects];
+//	//가로x세로x높이가 15x15x15인 정육면체 메쉬를 생성한다.
+//	CCubeMesh *pMesh = new CCubeMesh(pd3dDevice, 15.0f, 15.0f, 15.0f);
+//	CRotatingObject *pObject = new CRotatingObject();
+//	pObject->SetMesh(pMesh);
+//	pObject->SetShader(pShader);
+//	m_ppObjects[0] = pObject;
+//}
+
+void CScene::BuildObjects(ID3D11Device *pd3dDevice)
+{
+	m_nObjects = 1;
+	m_ppObjects = new CGameObject*[m_nObjects];
+
+	//텍스쳐 맵핑에 사용할 샘플러 상태 객체를 생성한다.
+	ID3D11SamplerState *pd3dSamplerState = NULL;
+	D3D11_SAMPLER_DESC d3dSamplerDesc;
+	ZeroMemory(&d3dSamplerDesc, sizeof(D3D11_SAMPLER_DESC));
+	d3dSamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	d3dSamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	d3dSamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	d3dSamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	d3dSamplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	d3dSamplerDesc.MinLOD = 0;
+	d3dSamplerDesc.MaxLOD = 0;
+	pd3dDevice->CreateSamplerState(&d3dSamplerDesc, &pd3dSamplerState);
+
+	// 파일로부터 텍스쳐 객체(ID3D11ShaderResourceView ; 뷰)를 생성한다.
+	ID3D11ShaderResourceView *pd3dTexture = NULL;
+	D3DX11CreateShaderResourceViewFromFile(pd3dDevice, _T("jungle.jpg"), NULL, NULL, &pd3dTexture, NULL);
+
+	CTexture *pJungleTexture = new CTexture(1);
+	pJungleTexture->SetTexture(0, pd3dTexture, pd3dSamplerState);
+
+	CShader *pTexturedLightingShader = new CTexturedLightingShader();
+	pTexturedLightingShader->CreateShader(pd3dDevice);				// 세계 행렬 및 재질 상수 버퍼를 생성한다.
+
+	CTexturedLightingCubeMesh *pCubeMesh = new CTexturedLightingCubeMesh(pd3dDevice, 15.0f, 15.0f, 15.0f);
+	CGameObject *pObject = new CGameObject();
+
+	// 다음의 재질은 빨간 색을 잘 반사한다.
+	CMaterial *pStandardMaterial = new CMaterial();				
+	pStandardMaterial->m_Material.m_d3dxcDiffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pStandardMaterial->m_Material.m_d3dxcAmbient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pStandardMaterial->m_Material.m_d3dxcSpecular = D3DXCOLOR(1.0f, 1.0f, 1.0f, 5.0f);
+	pStandardMaterial->m_Material.m_d3dxcEmissive = D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f);
+
+	pObject->SetMaterial(pStandardMaterial);
+	pObject->SetShader(pTexturedLightingShader);
+	pCubeMesh->SetTexture(pJungleTexture);
+	pObject->SetMesh(pCubeMesh);
+	m_ppObjects[0] = pObject;
+
+	CreateShaderVariables(pd3dDevice);							// 조명을 입력받는 상수 버퍼를 생성함과 동시에 조명 정보를 집어 넣는다.
+}
+
+void CScene::ReleaseObjects()
+{
+	ReleaseShaderVariables();
+	//게임 객체 리스트의 각 객체를 반환(Release)하고 리스트를 소멸시킨다.
+	if (m_ppObjects)
+	{
+		for (int j = 0; j < m_nObjects; j++) m_ppObjects[j]->Release();
+		delete[] m_ppObjects;
+	}
+}
+
+void CScene::CreateShaderVariables(ID3D11Device *pd3dDevice)
+{
+	m_pLights = new LIGHTS;
+	::ZeroMemory(m_pLights, sizeof(LIGHTS));
+	//게임 월드 전체를 비추는 주변조명을 설정한다.
+	m_pLights->m_d3dxcGlobalAmbient = D3DXCOLOR(0.1f, 0.1f, 0.1f, 1.0f);
+
+	//3개의 조명(점 광원, 스팟 광원, 방향성 광원)을 설정한다.
+	m_pLights->m_pLights[0].m_bEnable = 1.0f;
+	m_pLights->m_pLights[0].m_nType = POINT_LIGHT;
+	m_pLights->m_pLights[0].m_fRange = 300.0f;
+	m_pLights->m_pLights[0].m_d3dxcAmbient = D3DXCOLOR(0.1f, 0.0f, 0.0f, 1.0f);
+	m_pLights->m_pLights[0].m_d3dxcDiffuse = D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f);
+	m_pLights->m_pLights[0].m_d3dxcSpecular = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.0f);
+	m_pLights->m_pLights[0].m_d3dxvPosition = D3DXVECTOR3(300.0f, 300.0f, 300.0f);
+	m_pLights->m_pLights[0].m_d3dxvDirection = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_pLights->m_pLights[0].m_d3dxvAttenuation = D3DXVECTOR3(1.0f, 0.001f, 0.0001f);
+	m_pLights->m_pLights[1].m_bEnable = 1.0f;
+	m_pLights->m_pLights[1].m_nType = SPOT_LIGHT;
+	m_pLights->m_pLights[1].m_fRange = 100.0f;
+	m_pLights->m_pLights[1].m_d3dxcAmbient = D3DXCOLOR(0.1f, 0.1f, 0.1f, 1.0f);
+	m_pLights->m_pLights[1].m_d3dxcDiffuse = D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f);
+	m_pLights->m_pLights[1].m_d3dxcSpecular = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.0f);
+	m_pLights->m_pLights[1].m_d3dxvPosition = D3DXVECTOR3(500.0f, 300.0f, 500.0f);
+	m_pLights->m_pLights[1].m_d3dxvDirection = D3DXVECTOR3(0.0f, 0.0f, 1.0f);
+	m_pLights->m_pLights[1].m_d3dxvAttenuation = D3DXVECTOR3(1.0f, 0.01f, 0.0001f);
+	m_pLights->m_pLights[1].m_fFalloff = 8.0f;
+	m_pLights->m_pLights[1].m_fPhi = (float)cos(D3DXToRadian(40.0f));
+	m_pLights->m_pLights[1].m_fTheta = (float)cos(D3DXToRadian(20.0f));
+	m_pLights->m_pLights[2].m_bEnable = 1.0f;
+	m_pLights->m_pLights[2].m_nType = DIRECTIONAL_LIGHT;
+	m_pLights->m_pLights[2].m_d3dxcAmbient = D3DXCOLOR(0.1f, 0.1f, 0.1f, 1.0f);
+	m_pLights->m_pLights[2].m_d3dxcDiffuse = D3DXCOLOR(0.2f, 0.2f, 0.2f, 1.0f);
+	m_pLights->m_pLights[2].m_d3dxcSpecular = D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.0f);
+	m_pLights->m_pLights[2].m_d3dxvDirection = D3DXVECTOR3(0.0f, -1.0f, 0.0f);
+	m_pLights->m_pLights[3].m_bEnable = 1.0f;
+	m_pLights->m_pLights[3].m_nType = SPOT_LIGHT;
+	m_pLights->m_pLights[3].m_fRange = 60.0f;
+	m_pLights->m_pLights[3].m_d3dxcAmbient = D3DXCOLOR(0.1f, 0.0f, 0.0f, 1.0f);
+	m_pLights->m_pLights[3].m_d3dxcDiffuse = D3DXCOLOR(0.5f, 0.0f, 0.0f, 1.0f);
+	m_pLights->m_pLights[3].m_d3dxcSpecular = D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.0f);
+	m_pLights->m_pLights[3].m_d3dxvPosition = D3DXVECTOR3(500.0f, 300.0f, 500.0f);
+	m_pLights->m_pLights[3].m_d3dxvDirection = D3DXVECTOR3(0.0f, -1.0f, 0.0f);
+	m_pLights->m_pLights[3].m_d3dxvAttenuation = D3DXVECTOR3(1.0f, 0.01f, 0.0001f);
+	m_pLights->m_pLights[3].m_fFalloff = 20.0f;
+	m_pLights->m_pLights[3].m_fPhi = (float)cos(D3DXToRadian(40.0f));
+	m_pLights->m_pLights[3].m_fTheta = (float)cos(D3DXToRadian(15.0f));
+
+	D3D11_BUFFER_DESC d3dBufferDesc;
+	ZeroMemory(&d3dBufferDesc, sizeof(d3dBufferDesc));
+	d3dBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	d3dBufferDesc.ByteWidth = sizeof(LIGHTS);
+	d3dBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	d3dBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	D3D11_SUBRESOURCE_DATA d3dBufferData;
+	ZeroMemory(&d3dBufferData, sizeof(D3D11_SUBRESOURCE_DATA));
+	d3dBufferData.pSysMem = m_pLights;
+	pd3dDevice->CreateBuffer(&d3dBufferDesc, &d3dBufferData, &m_pd3dcbLights);
+}
+
+void CScene::ReleaseShaderVariables()
+{
+	if (m_pLights) delete m_pLights;
+	if (m_pd3dcbLights) m_pd3dcbLights->Release();
+}
+
+void CScene::UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, LIGHTS *pLights)
+{
+	D3D11_MAPPED_SUBRESOURCE d3dMappedResource;
+	pd3dDeviceContext->Map(m_pd3dcbLights, 0, D3D11_MAP_WRITE_DISCARD, 0, &d3dMappedResource);
+	LIGHTS *pcbLight = (LIGHTS *)d3dMappedResource.pData;
+	memcpy(pcbLight, pLights, sizeof(LIGHTS));
+	pd3dDeviceContext->Unmap(m_pd3dcbLights, 0);
+	pd3dDeviceContext->PSSetConstantBuffers(PS_SLOT_LIGHT, 1, &m_pd3dcbLights);
+}
+
+bool CScene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+	return(false);
+}
+
+bool CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+	return(false);
+}
+
+bool CScene::ProcessInput()
+{
+	return(false);
+}
+
+void CScene::AnimateObjects(float fTimeElapsed)
+{
+	for (int i = 0; i < m_nObjects; i++);// m_ppObjects[i]->Animate(fTimeElapsed);
+}
+
+
+//void CScene::Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera)
+//{
+//	for (int i = 0; i < m_nObjects; i++)
+//	{
+//		m_ppObjects[i]->Render(pd3dDeviceContext);
+//	}
+//}
+
+//void CScene::Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera)
+//{
+//	if (m_pLights && m_pd3dcbLights) UpdateShaderVariable(pd3dDeviceContext, m_pLights);
+//
+//	for (int i = 0; i < m_nShaders; i++)
+//	{
+//		m_ppShaders[i]->Render(pd3dDeviceContext, pCamera);
+//	}
+//}
+
+void CScene::Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera)
+{
+	if (m_pLights && m_pd3dcbLights) UpdateShaderVariable(pd3dDeviceContext, m_pLights);
+
+	for (int i = 0; i < m_nObjects; i++)
+	{
+		m_ppObjects[i]->Render(pd3dDeviceContext);
+	}
+}
