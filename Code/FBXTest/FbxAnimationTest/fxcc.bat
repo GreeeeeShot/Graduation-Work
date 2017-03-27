aec7f8e192409a4c5c4c5c3d76c3d5e2c4ab0271
GreeeeeShot <joonbum638@naver.com>
2017-03-28 03:03:28 +0900

    FBXTest 통폐합 | 같은 작업 환경에서 텍스쳐 맵핑은 되는 것을 확인
    맵핑자체는 안맞는듯

--- Code/FBXTest/FbxAnimationTest/fxcc.bat
+++
@@ -1,5 +0,0 @@
-set PATH=c:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\
-	cd ./
-	fxc /Od /Zi /Ges /T vs_5_0 /E VS /Fo VS.fxo Effect.fx
-	fxc /Od /Zi /Ges /T ps_5_0 /E PS /Fo PS.fxo Effect.fx
-	cd ..
