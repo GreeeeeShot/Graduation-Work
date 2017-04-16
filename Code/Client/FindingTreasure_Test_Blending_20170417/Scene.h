f5e864a7d5f5a165bba02905411ca632f053c52c
batherit <batherit0703@naver.com>
2017-04-17 03:35:48 +0900

    내부 컬링 개선 및 수평면 컬링을 통한 성능 개선 (약 40 프레임 확보)
    정정.
    - 이전 절두체 컬링에서 유동 0~150 프레임이라 하였는데, 0~200 프레임 정도입니다.
    
    상세.
    1. 내부 컬링 개선
    - 이전에는 해당 복셀의 상하 좌우 앞뒤 총 6방향을 탐색하여, 모두 막힌 경우 그리지 않는 것으로 하였습니다.
    - 현재도 여섯 방향을 탐색하지만, 달라진 점은 꼭 6방향 모두를 탐색할 필요는 없다는 것입니다. 즉, 최소 3방향 탐색만 하고도
    내부에 있는지를 알 수 있습니다.
    - 이것은 각 복셀 면의 법선 벡터와 복셀 위치에서 카메라로의 방향 벡터(방향벡터)간 내적을 통해 0.0f이상 1.0f이하일 때,
    내부 복셀인지를 판별하기 시작합니다.
    - 일반적으로, 방향 벡터는 복셀 면에 평행하지 않으므로, 복셀의 3개의 면이 보인다고 할 수 있으며, 이 면에 대해서만 내부
    복셀인지를 판별하기 시작합니다.
    
    2. 수평면 컬링
    - 내부 컬링 함수에서 작동합니다.
    - 방향 벡터와 검사하고자 하는 복셀 면의 법선 벡터가 수직에 가깝지 않으면, 조건을 보고 렌더를 합니다.
    - 수직에 가까우면, 보이는 면적이 적다는 뜻이므로 이를 그리지 않아도 큰 지장이 없을 것 같았습니다.
    
    프레임 확보 결과
    - 이전 55~220 프레임
    - 이후 90~260 프레임

---
+++ Code/Client/FindingTreasure_Test_Blending_20170417/Scene.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Object.h"			// 씬의 구성요소로 오브젝트들이 존재한다.
+#include "Camera.h"
+#include "VoxelTerrain.h"
+#include "LightResource.h"
+
+#define GRAVITATIONAL_ACCELERATION			-9.8f	// m/s^2
+#define AIR_RESISTANCE_COEFFICIENT			10.0f//4.6f
+
+class CScene
+{
+private:
+	/*CGameObject **m_ppObjects;*/
+	CPlayer **m_ppPlayers;
+
+	int m_nObjects;
+
+	LIGHTS *m_pLights;
+	ID3D11Buffer *m_pd3dcbLights;
+
+	
+	CWaveObject *m_pWaveEffect;
+	
+	float m_fGravitationalAcceleration;
+
+public:
+	CVoxelTerrain *m_pVoxelTerrain;
+	CScene();
+	~CScene();
+
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	void BuildObjects(ID3D11Device *pd3dDevice);
+	void SetPlayers(CPlayer **ppPlayers);
+	void ReleaseObjects();
+
+	void CreateShaderVariables(ID3D11Device *pd3dDevice);
+	void UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, LIGHTS *pLights);
+	void ReleaseShaderVariables();
+
+	bool ProcessInput();
+	void AnimateObjects(ID3D11DeviceContext*pd3dDeviceContext, float fTimeElapsed);
+	void Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera);		// 카메라 절두체 기법을 위하여 매개변수로 pCamera가 존재한다.
+};
+
+
---
+++ Code/Client/FindingTreasure_Test_Blending_20170417/VoxelTerrain.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "Shader.h"
+#include "Object.h"
+#include "Camera.h"
+#include "Voxel.h"
+
+#define BLUEPRINT_LAYER						50	// 개
+#define BLUEPRINT_ROW						100	// 개
+#define BLUEPRINT_COL						100	// 개
+
+//설계도에 대한 정보를 담기위한 구조체
+class CBlueprint
+{
+public:
+	CBlueprint();
+	~CBlueprint();
+
+	int m_iBlueprint[BLUEPRINT_LAYER][BLUEPRINT_ROW][BLUEPRINT_COL];
+	int m_nVoxelObjectsNum;
+	int m_nMaxLayer;
+	int m_nMaxCol;
+	int m_nMaxRow;
+	float m_fCubeWidth;
+	float m_fCubeHeight;
+	float m_fCubeDepth;
+	float m_fOffsetXFromLocal;
+	float m_fOffsetYFromLocal;
+	float m_fOffsetZFromLocal;
+
+	// txt로 저장된 맵 파일을 가져온다.
+	CBlueprint* LoadBlueprint(const char *strBlueprintFileName);
+};
+
+class CVoxelTerrain
+{
+private:
+	int m_nVoxelObjectsNum;
+	D3DXMATRIX m_d3dxmtxWorld;
+	CShader *m_pShader;
+
+public:
+	CVoxelTerrain();
+	~CVoxelTerrain();
+
+	int m_iMaxLayer;
+	int m_iMaxRow;
+	int m_iMaxCol;
+
+	int m_iLayerStride;
+	int m_iRowStride;
+
+	float m_fOffsetXFromLocal;
+	float m_fOffsetYFromLocal;
+	float m_fOffsetZFromLocal;
+
+	float m_fCubeHeight;
+	float m_fCubeWidth;
+	float m_fCubeDepth;
+
+	CVoxel** m_ppVoxelObjectsForCollision;
+	//CVoxel** m_ppVoxelObjectsForRendering;
+	std::vector<CVoxel*> m_vecVoxelObjectsForRendering[VOXEL_TYPE_NUM];		// 0번째 인덱스는 사용하지 않는다.
+
+	D3DXMATRIX m_d3dxmtxFromIdxToPos;
+	D3DXMATRIX m_d3dxmtxFromPosToIdx;
+
+	void CreateVoxelTerrain(CBlueprint* pBlueprint);
+	void SetPosition(float x, float y, float z);
+	void SetPosition(D3DXVECTOR3 d3dxvPosition);
+	CVoxel* GetVoxelForType(VOXEL_TYPE eVoxelType);
+	D3DXVECTOR3 GetPosition();
+	bool IsInnerVoxel(CVoxel* pVoxel, CCamera* pCamera);
+	void Render(ID3D11DeviceContext *pd3dDeviceContext, CCamera *pCamera);
+};
+
