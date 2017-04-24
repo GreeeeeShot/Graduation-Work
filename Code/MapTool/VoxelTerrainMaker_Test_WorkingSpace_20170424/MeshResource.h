d83038432b23e6adcdabdae8d372ad2109989052
batherit <batherit0703@naver.com>
2017-04-24 22:20:18 +0900

    맵툴 기능 추가 : 뷰 모드, 생성된 작업 공간 크기 변경, 작업 공간 내 복셀 터레인 위치 변경
    상세.
    1. 뷰 모드
    - 전체 뷰 :  현재 생성된 모든 복셀을 렌더합니다.
    - 누적 층 뷰 : 보조 판넬부터 그 아래의 모든 복셀을 렌더합니다.
    - 해당 층 뷰 : 보조 판넬이 위치하는 층의 모든 복셀을 렌더합니다.
    
    2. 생성된 작업 공간 크기 변경
    - 작업 공간의 크기를 변경할 수 있습니다.
    - 작업 공간의 크기가 기존보다 작아질 경우에 한 해, 복셀 터레인을 이루는 어떤 복셀과 충돌하게 된다면, 크기 변경이 되지
    않습니다.
    3. 작업 공간 내 복셀 터레인 위치 변경
    - 작업 공간 안에서 스페이스 키와 화살표 키의 조합으로 복셀 터레인의 위치를 변경할 수 있습니다.
    - 단, 복셀 터레인을 이루는 어떤 복셀과 충돌하게 된다면, 위치 변경이 되지 않습니다.
    - 키 조작.
    ↑ : +z축으로 복셀 터레인 이동
    ↓ : -z 축으로 복셀 터레인 이동
    ← : -x 축으로 복셀 터레인 이동
    → : +x 축으로 복셀 터레인 이동
    space + ↑ : +y축으로 복셀 터레인 이동
    space + ↓ : -y축으로 복셀 터레인 이동

---
+++ Code/MapTool/VoxelTerrainMaker_Test_WorkingSpace_20170424/MeshResource.h
@@ -0,0 +1,186 @@
+#pragma once
+#include "LightResource.h"
+#include "TextureResource.h"
+#include "PhysicalCollision.h"
+
+enum VOXEL_SLOPE_DIR { VOXEL_SLOPE_DIR_FRONT, VOXEL_SLOPE_DIR_RIGHT, VOXEL_SLOPE_DIR_REAR, VOXEL_SLOPE_DIR_LEFT };
+
+class CCubeFaceNormal
+{
+public:
+	D3DXVECTOR3 m_d3dxLeftNormal;
+	D3DXVECTOR3 m_d3dxRightNormal;
+	D3DXVECTOR3 m_d3dxFrontNormal;
+	D3DXVECTOR3 m_d3dxRearNormal;
+	D3DXVECTOR3 m_d3dxTopNormal;
+	D3DXVECTOR3 m_d3dxBottomNormal;
+
+	CCubeFaceNormal();
+	~CCubeFaceNormal();
+};
+
+class CCubeSlopeFace
+{
+public:
+	D3DXVECTOR3 m_d3dxvUpNegative;
+	D3DXVECTOR3 m_d3dxvUpPositive;
+	D3DXVECTOR3 m_d3dxvDownNegative;
+	D3DXVECTOR3 m_d3dxvDownPositive;
+
+	CCubeSlopeFace();
+	~CCubeSlopeFace();
+};
+
+class CMesh
+{
+public:
+	//CMesh 클래스의 생성자와 소멸자를 선언한다. 
+	CMesh(ID3D11Device *pd3dDevice);
+	virtual ~CMesh();
+
+	//CMesh 클래스 객체의 참조(Reference)와 관련된 멤버 변수와 함수를 선언한다.
+private:
+	int m_nReferences;
+
+public:
+	void AddRef();
+	void Release();
+
+protected:
+	//정점의 요소들을 나타내는 버퍼들을 입력조립기에 전달하기 위한 버퍼이다. 
+	ID3D11Buffer **m_ppd3dVertexBuffers;
+
+	int m_nVertices;
+	D3DXVECTOR3 * m_pd3dxvPositions;
+	//정점의 위치 벡터를 저장하기 위한 버퍼에 대한 인터페이스 포인터이다. 
+	ID3D11Buffer *m_pd3dPositionBuffer;
+
+	//정점을 조립하기 위해 필요한 버퍼의 개수이다. 
+	int m_nBuffers;
+	//정점의 요소들을 나타내는 버퍼들의 원소의 바이트 수를 나타내는 배열이다. 
+	UINT *m_pnVertexStrides;
+	//정점의 요소들을 나타내는 버퍼들의 시작 위치(바이트 수)를 나타내는 배열이다. 
+	UINT *m_pnVertexOffsets;
+
+	/*인덱스 버퍼(인덱스의 배열)에 대한 인터페이스 포인터이다. 인덱스 버퍼는 정점 버퍼(배열)에 대한 인덱스를 가진다.*/
+	ID3D11Buffer *m_pd3dIndexBuffer;
+	//인덱스 버퍼가 포함하는 인덱스의 개수이다. 
+	UINT m_nIndices;
+	//인덱스 버퍼에서 메쉬를 표현하기 위해 사용되는 시작 인덱스이다. 
+	UINT m_nStartIndex;
+	//각 인덱스에 더해질 인덱스이다. 
+	int m_nBaseVertex;
+
+	//정점 데이터가 어떤 프리미티브를 표현하고 있는 가를 나타내는 멤버 변수를 선언한다.
+	D3D11_PRIMITIVE_TOPOLOGY m_d3dPrimitiveTopology;
+
+	/*래스터라이저 상태 객체에 대한 인터페이스 포인터를 선언한다. 래스터라이저 상태 객체는 “CGameObject" 클래스의 멤버로 추가하여도 된다. 래스터라이저 상태 객체가 메쉬의 멤버일 때와 게임 객체의 멤버일 때의 의미상의 차이를 생각해보기를 바란다.*/
+	ID3D11RasterizerState *m_pd3dRasterizerState;
+
+	//정점 데이터를 렌더링하는 멤버 함수를 선언한다.
+public:
+	AABB m_AABB;
+	CCubeFaceNormal m_CubeFaceNormal;
+	CCubeSlopeFace m_CubeSlopeFace;
+
+	virtual void SetRasterizerState(ID3D11Device *pd3dDevice);
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+};
+
+class CDiffusedMesh : public CMesh
+{
+public:
+	CDiffusedMesh(ID3D11Device *pd3dDevice);
+	virtual ~CDiffusedMesh();
+};
+
+/* -------------------<<<<<< CDiffusedCoordSystemMesh >>>>>>------------------- */
+class CDiffusedCoordSystemMesh : public CDiffusedMesh
+{
+public :
+	CDiffusedCoordSystemMesh(
+		ID3D11Device *pd3dDevice,
+		float AxisXLength = 10.0f, float AxisYLength = 10.0f, float AxisZLength = 10.0f,
+		D3DXCOLOR AxisXColor = D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f),
+		D3DXCOLOR AxisYColor = D3DXCOLOR(0.0f, 0.0f, 1.0f, 1.0f),
+		D3DXCOLOR AxisZColor = D3DXCOLOR(0.0f, 1.0f, 0.0f, 1.0f)
+	);
+	virtual ~CDiffusedCoordSystemMesh();
+};
+
+/* -------------------<<<<<< CDiffusedWorkingSpaceMesh >>>>>>------------------- */
+class CDiffusedWorkingSpaceMesh : public CDiffusedMesh
+{
+public: 
+	CDiffusedWorkingSpaceMesh(ID3D11Device *pd3dDevice, float fWidth = 1.0f, float fHeight = 1.0f, float fDepth = 1.0f);
+	virtual ~CDiffusedWorkingSpaceMesh();
+
+	//virtual void SetRasterizeState(ID3D11Device *pd3dDevice);
+};
+
+/* -------------------<<<<<< CDiffusedWorkingBoardMesh >>>>>>------------------- */
+class CDiffusedWorkingBoardMesh : public CDiffusedMesh
+{
+public:
+	CDiffusedWorkingBoardMesh(ID3D11Device *pd3dDevice, int iWidthCount, float fWidthIntervalUnit, int iDepthCount, float fDepthIntervalUnit);
+	virtual ~CDiffusedWorkingBoardMesh();
+
+	//virtual void SetRasterizeState(ID3D11Device *pd3dDevice);
+};
+
+class CLightingMesh : public CMesh
+{
+public:
+	CLightingMesh(ID3D11Device *pd3dDevice);
+	virtual ~CLightingMesh();
+
+protected:
+	//조명의 영향을 계산하기 위하여 법선벡터가 필요하다.
+	ID3D11Buffer *m_pd3dNormalBuffer;
+
+public:
+	void CalculateVertexNormal(D3DXVECTOR3 *pd3dxvNormals, D3DXVECTOR3 *pd3dxvVertices, UINT *piIndices);
+};
+
+/* -------------------<<<<<< CTexturedLightingCubeMesh >>>>>>------------------- */
+class CTexturedLightingCubeMesh : public CLightingMesh
+{
+public:
+	CTexturedLightingCubeMesh(ID3D11Device *pd3dDevice, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f);
+	virtual ~CTexturedLightingCubeMesh();
+
+	virtual void SetRasterizerState(ID3D11Device *pd3dDevice);
+	//virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+};
+
+class CTexturedLightingUphillMesh : public CLightingMesh
+{
+public:
+	CTexturedLightingUphillMesh(ID3D11Device *pd3dDevice, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f, VOXEL_SLOPE_DIR eVoxelDir = VOXEL_SLOPE_DIR_FRONT);
+	virtual ~CTexturedLightingUphillMesh();
+
+	virtual void SetRasterizerState(ID3D11Device *pd3dDevice);
+	//virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+};
+
+class CMeshResource
+{
+public:
+	CMeshResource();
+	~CMeshResource();
+
+	static CMesh *pPirateMesh;
+	static CMesh *pSnowWomanMesh;
+
+	static CMesh *pLocalCoordMesh;
+	static CMesh *pWorldCoordMesh;
+
+	static CMesh **ppRegisteredVoxelMesh;
+	static CMesh *pStandardVoxelMesh;
+	static CMesh *pUphill000Mesh;
+	static CMesh *pUphill090Mesh;
+	static CMesh *pUphill180Mesh;
+	static CMesh *pUphill270Mesh;
+
+	static void CreateMeshResource(ID3D11Device *pd3dDevice);
+};
