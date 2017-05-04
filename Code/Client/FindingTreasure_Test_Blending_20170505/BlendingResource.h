fc1819ad1582ae5a1d28aaf33773f540c0f8779e
batherit <batherit0703@naver.com>
2017-05-05 04:37:41 +0900

    오르막길에 대한 3가지 버그 수정
    상세.
    1. 프레임이 낮을 때, 통과하지 말아야 할 복셀을 통과하여 충돌체크가 꼬이는 경우
    -> 현재 위치를 기준으로 복셀과 충돌하지 않는 순간까지 위로 올림으로써 해결
    2. 오르막길 바로 다음에 직육면체 복셀이 있을 때 그 복셀을 뚫는 경우
    -> 중간 충돌 피봇점을 생성하고 이를 통해 충돌 체크함으로써 해결
    3. 내려갈 때, 오르막길 아래에 존재하는 직육면체 복셀에 걸리는 경우
    -> 주위 충돌 체크 후 다시 아래를 확인하여 오르막길 복셀일 경우, 아래 주위 충돌 체크를 하지 않음으로써 해결
    
    그 외.
    - 3가지 버그를 해결하는 도중 주위 충돌 체크에서 새로운 버그를 발견하였으나, 내적을 통해 실제 충돌이 이루어진 것인지를
    판단함으로써 이를 해결.
    - 대부분의 버그를 해결한 듯 보이나, 충분한 시간이 흘러야 안정화될 듯 함.

---
+++ Code/Client/FindingTreasure_Test_Blending_20170505/BlendingResource.h
@@ -0,0 +1,31 @@
+#pragma once
+//
+//class CBlending
+//{
+//public:
+//	
+//	//CMesh 클래스의 생성자와 소멸자를 선언한다. 
+//	CBlending(ID3D11Device *pd3dDevice);
+//	virtual ~CBlending();
+//};
+//
+//class CTransparentBlending : public CBlending
+//{
+//public:
+//	CTransparentBlending(ID3D11Device *pd3dDevice);
+//	virtual ~CTransparentBlending();
+//
+//
+//};
+
+class CBlendingResource
+{
+public:
+	CBlendingResource();
+	~CBlendingResource();
+
+	static ID3D11BlendState *pTransparentBlending;
+
+	static void CreateBlendingResource(ID3D11Device *pd3dDevice);
+};
+
