fadfdfc4f86acfb9e569c967857cecb6b9061560
batherit <batherit0703@naver.com>
2017-07-27 05:03:06 +0900

    라이트 셰프트 효과 보류, 광원 객체 분리, 광원 절두체 컬링 구현, 보물 상자 들고 가기, 충돌 시 떨굼 구현
    상세.
    1. 라이트 셰프트 효과 보류
    - 멀티 렌더 타겟 쪽에서 계속 실패(렌더 X)가 나서 해당 효과 구현은 잠정 보류하기로 결정.
    
    2. 광원 객체 분리
    - 쉐도우 렌더러 클래스에 종속적이었던 광원 객체를 포인터로 연결시키도록 함
    - 따라서, 쉐도우 렌더러가 없어도 라이트 셰프트 효과만 구현하는 것이 가능해짐. (그러나 이 효과는 현재 위에 설명하였듯 안되고
    있음.)
    
    3. 광원 절두체 컬링 구현.
    - 쉐도우 렌더러에서 광원은 일종의 카메라와 동일한 좌표계 변환 과정을 거치므로, 동일하게 깊이 갱신 과정에서 절두체 컬링도
    가능함. 고로 이것을 구현함.
    - 하지만, 실제로는 현재 모든 복셀을 광원이 비추고 있으므로 실효성은 없다고 볼 수 있음. 하지만 부분적으로 비추는 상황이 될
    경우, 성능이 개선될 수 있음.
    
    4. 보물 상자 들고 가기
    - 이전에는 보물 상자를 옮기기 위하여 몸으로 부딫치거나, 미는 수단 밖에 없었음. 고로 이동하기가 매우 힘들고 불편하였음.
    - 그렇기에 이것을 들고 가게 함으로써 답답함을 해소함.
    - 보물 상자를 들고 있는 동안에는 복셀 파기 및 설치가 불가능함.
    - 하지만 거침없이 가면 재미가 없으므로, 질량 추가에 대한 감속 및 점프력 약화 그리고 스태미너 개념을 추가할 예정임.
    // 추가적으로 들려진 보물 상자의 회전은 들고 있는 케릭터 회전에 종속적임.
    
    5. 충돌 시 떨굼 구현.
    - 보물 상자를 들고 있는 케릭터가 복셀이나 다른 케릭터와 충돌하더라도 보물 상자를 떨구지 않음.
    - 하지만, 들려진 보물 상자가 다른 케릭터나 복셀에 충돌하게 되면 땅에 떨어지게 만듦.
    // 이때, 보물상자의 속도는 들고 있는 케릭터 속도에 종속적임.

---
+++ Code/FindingTreasure/FindingTreasure_Test_Blending_20170727/GameUI.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Player.h"
+#include "Sprite.h"
+
+class CGameUI
+{
+public:
+	CCamera *m_pCamera;
+	CGameObject *m_pVoxelPocketUI;
+	CSprite *m_pVoxelIcons;
+	CGameObject *m_pVoxelActivated;
+	CGameObject *m_pRespawningTextUI;
+	CGameObject *m_pRespawningGaugeUI;
+	//CPlayer *pMyPlayer;
+
+	CGameUI();
+	~CGameUI();
+	
+	void SetCamera(CCamera *pCamera);
+	//void SetMyPlayer(CPlayer *pMyPlayer);
+	void BuildUIObjects(ID3D11Device *pd3dDevice);
+	void BackgroundUIRender(ID3D11DeviceContext *pd3dDeviceContext);
+	void PlayerHavingVoxelRender(ID3D11DeviceContext *pd3dDeviceCotext, CPlayer *pPlayer);
+	void RespawningRender(ID3D11DeviceContext *pd3dDeviceCotext, CRespawnManager* pRespawnManager, CPlayer *pPlayer);
+};
+
---
+++ Code/FindingTreasure/FindingTreasure_Test_Blending_20170727/VertexResource.h
@@ -0,0 +1,82 @@
+#pragma once
+
+// 메쉬를 구성하는 정점에 대한 클래스이다.
+class CVertex
+{
+	//정점의 위치 정보(3차원 벡터)를 저장하기 위한 멤버 변수를 선언한다. 
+	D3DXVECTOR3 m_d3dxvPosition;
+
+public:
+	//CVertex 클래스의 생성자와 소멸자를 다음과 같이 선언한다. 
+	CVertex() { m_d3dxvPosition = D3DXVECTOR3(0, 0, 0); }
+	CVertex(D3DXVECTOR3 d3dxvPosition) { m_d3dxvPosition = d3dxvPosition; }
+	D3DXVECTOR3 GetPositiion(void) { return m_d3dxvPosition; }
+	~CVertex() { }
+};
+
+class CDiffusedVertex		// 색상을 가지는 정점이다.
+{
+	D3DXVECTOR3 m_d3dxvPosition;
+	//정점의 색상을 나타내는 멤버 변수(D3DXCOLOR 구조체)를 선언한다. 
+	D3DXCOLOR m_d3dxcDiffuse;	//정점에 색상 정보가 추가되었다.
+public:
+	//생성자와 소멸자를 선언한다.
+	CDiffusedVertex(float x, float y, float z, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex(D3DXVECTOR3 d3dxvPosition, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = d3dxvPosition; m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxcDiffuse = D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.0f); }
+	~CDiffusedVertex() { }
+};
+
+class CTexturedVertex
+{
+public:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR2 m_d3dxvTexCoord;
+
+	CTexturedVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvTexCoord = D3DXVECTOR2(0.0f, 0.0f); }
+	CTexturedVertex(float x, float y, float z, float u, float v) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxvTexCoord = D3DXVECTOR2(u, v); }
+	CTexturedVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR2 d3dxvTexture) { m_d3dxvPosition = d3dxvPosition; m_d3dxvTexCoord = d3dxvTexture; }
+	~CTexturedVertex() { }
+};
+
+class CTexturedLightingVertex
+{
+public:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR3 m_d3dxvNormal;
+	D3DXVECTOR2 m_d3dxvTexCoord;
+
+	CTexturedLightingVertex(float x, float y, float z, float nx, float ny, float nz, float u, float v) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxvNormal = D3DXVECTOR3(nx, ny, nz); m_d3dxvTexCoord = D3DXVECTOR2(u, v); }
+	CTexturedLightingVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR3 d3dxvNormal, D3DXVECTOR2 d3dxvTexCoord) { m_d3dxvPosition = d3dxvPosition; m_d3dxvNormal = d3dxvNormal; m_d3dxvTexCoord = d3dxvTexCoord; }
+	CTexturedLightingVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvNormal = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxvTexCoord = D3DXVECTOR2(0.0f, 0.0f); }
+	~CTexturedLightingVertex() { }
+};
+
+class CNormalMappingVertex
+{
+public:
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXVECTOR3 m_d3dxvNormal;
+	D3DXVECTOR3 m_d3dxvTexCoord;
+	D3DXVECTOR3 m_d3dxvTBasis;
+
+	CNormalMappingVertex(float x, float y, float z, float nx, float ny, float nz, float u, float v, float tx, float ty, float tz) { 
+		m_d3dxvPosition = D3DXVECTOR3(x, y, z); 
+		m_d3dxvNormal = D3DXVECTOR3(nx, ny, nz); 
+		m_d3dxvTexCoord = D3DXVECTOR3(u, v, 0.0f); 
+		m_d3dxvTBasis = D3DXVECTOR3(tx, ty, tz);
+	}
+	CNormalMappingVertex(D3DXVECTOR3 d3dxvPosition, D3DXVECTOR3 d3dxvNormal, D3DXVECTOR3 d3dxvTexCoord, D3DXVECTOR3 d3dxvTBasis) { 
+		m_d3dxvPosition = d3dxvPosition; 
+		m_d3dxvNormal = d3dxvNormal;
+		m_d3dxvTexCoord = d3dxvTexCoord;
+		m_d3dxvTBasis = d3dxvTBasis;
+	}
+	CNormalMappingVertex() { 
+		m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); 
+		m_d3dxvNormal = D3DXVECTOR3(0.0f, 0.0f, 0.0f); 
+		m_d3dxvTexCoord = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		m_d3dxvTBasis = D3DXVECTOR3(1.0f, 0.0f, 0.0f);
+	}
+	~CNormalMappingVertex() { }
+};
